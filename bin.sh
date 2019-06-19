c3d9b3a6b5720282aac666e12ec8d86fa0f023b7
Sayang99 <51100872+Sayang99@users.noreply.github.com>
2019-06-20 02:09:09 +0700

    Add files via upload

---
+++ bin.sh
@@ -0,0 +1,181 @@
+#usr/bin/bash
+clear
+#ini warna
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# Recode ajh mmnx
+echo
+echo
+#ini paket untuk menjalankan tools
+bin.sh
+echo
+clear
+echo
+echo
+echo $me"                     ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶"
+echo $me"                 ¶¶¶¶¶¶             ¶¶¶¶¶¶¶"
+echo $me"              ¶¶¶¶                        ¶¶¶¶"
+echo $me"             ¶¶¶                            ¶¶¶"
+echo $me"            ¶¶                                ¶¶"
+echo $me"           ¶¶                                  ¶¶"
+echo $me"          ¶¶                                   ¶¶"
+echo $me"          ¶¶´¶¶                             ¶¶´¶¶"
+echo $me"          ¶¶´¶¶                             ¶¶´´¶"
+echo $me"          ¶¶´¶¶                             ¶¶´´¶"
+echo $me"         ¶¶  ¶¶                            ¶¶´¶¶"
+echo $me"          ¶¶´´¶¶                           ¶¶´´¶¶"
+echo $me"           ¶¶´¶¶   ¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶   ¶¶´¶¶"
+echo $me"            ¶¶¶¶´¶¶¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶¶¶ ¶¶¶¶¶"
+echo $me"             ¶¶¶´¶¶¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶¶¶ ¶¶¶"
+echo $me"    ¶¶¶       ¶¶  ¶¶¶¶¶¶¶¶       ¶¶¶¶¶¶¶¶¶  ¶¶      ¶¶¶¶"
+echo $me"   ¶¶¶¶¶     ¶¶   ¶¶¶¶¶¶¶   ¶¶¶   ¶¶¶¶¶¶¶   ¶¶     ¶¶¶¶¶¶"
+echo $me"  ¶¶   ¶¶    ¶¶     ¶¶¶    ¶¶¶¶¶    ¶¶¶     ¶¶    ¶¶   ¶¶"
+echo $me" ¶¶¶    ¶¶¶¶  ¶¶          ¶¶¶¶¶¶¶          ¶¶  ¶¶¶¶    ¶¶¶"
+echo $me"¶¶         ¶¶¶¶¶¶¶¶       ¶¶¶¶¶¶¶        ¶¶¶¶¶¶¶¶¶        ¶¶"
+echo $me"¶¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶    ¶¶¶¶¶¶¶    ¶¶¶¶¶¶¶¶      ¶¶¶¶¶¶¶¶"
+echo $me"  ¶¶¶¶ ¶¶¶¶¶      ¶¶¶¶¶              ¶¶¶ ¶¶     ¶¶¶¶¶¶ ¶¶¶"
+echo $me"       ¶¶¶¶¶¶  ¶¶¶  ¶¶           ¶¶  ¶¶¶  ¶¶¶¶¶¶"        
+echo $me"              ¶¶¶¶¶¶ ¶¶ ¶¶¶¶¶¶¶¶¶¶¶ ¶¶ ¶¶¶¶¶¶"
+echo $me"                  ¶¶ ¶¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶¶"
+echo $me"                ¶¶¶¶  ¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶   ¶¶¶¶¶"
+echo $me"            ¶¶¶¶¶ ¶¶   ¶¶¶¶¶¶¶¶¶¶¶¶¶   ¶¶ ¶¶¶¶¶"
+echo $me"    ¶¶¶¶¶¶¶¶¶¶     ¶¶                 ¶¶      ¶¶¶¶¶¶¶¶¶"
+echo $me"   ¶¶           ¶¶¶¶¶¶¶             ¶¶¶¶¶¶¶¶          ¶¶"
+echo $me"    ¶¶¶     ¶¶¶¶¶     ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶     ¶¶¶¶¶     ¶¶¶"
+echo $me"      ¶¶   ¶¶¶           ¶¶¶¶¶¶¶¶¶           ¶¶¶   ¶¶"
+echo $me"      ¶¶  ¶¶                                   ¶¶  ¶¶"
+echo $me"       ¶¶¶¶                                     ¶¶¶¶"
+echo
+echo
+echo $cy "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo $me "╔─━━━━━━━★━━━━━━━─╗"
+echo $i     "๏SCRIPT BY : E Z 01๏"
+echo $me "╚─━━━━━━━★━━━━━━━─╝"
+echo $cy "╔─━━━━━━━★━━━━━━━─╗"
+echo $i     "๏WA : 082288907664๏"
+echo $cy "╚─━━━━━━━★━━━━━━━─╝"
+echo $bi "╔─━━━━━━━★━━━━━━━─╗"
+echo $i    "๏YOUTUBE  : E Z 01๏"
+echo $bi "╚─━━━━━━━★━━━━━━━─╝"
+echo $cy "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo
+echo
+echo $cy "╔─━━━━━━━★━━━━━━━─╗"
+echo $i     "๏DAFTAR TOOL๏"
+echo $cy "╚─━━━━━━━★━━━━━━━─╝"
+echo 
+echo $me "╔─━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╗"
+echo     $i  "Brute FORCE FB"
+echo $me "╚─━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╝"
+echo $me "╔─━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╗"
+echo     $i  "Banggunin Sahur"
+echo $me "╚─━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╝"
+echo $me "╔─━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╗"
+echo     $i  "Skull PROJECT"
+echo $me "╚─━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╝"
+echo $me "╔─━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╗"
+echo     $i  "Bot ALICE (KHUSUS JOMBLO)"
+echo $me "╚─━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╝"
+echo $me "╔─━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╗"
+echo     $i  "MATA MATA"
+echo $me "╚─━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╝"
+echo $me "╔─━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╗"
+echo     $i  "Trap PROJECT V1(BETA)"
+echo $me "╚─━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╝"
+echo $me "╔─━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╗"
+echo     $i  "Sadap CCTV"
+echo $me "╚─━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╝"
+echo $me "╔─━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╗"
+echo     $i  "E X I T"
+echo $me "╚─━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╝"
+echo
+echo
+echo $me "╔─━━━━━━━━─╗"
+echo     $i  "PILIH NOMOR.?"
+echo $me "╚─━━━━━━━━─╝"
+read -p">>>" pil;
+
+if [ $pil = 1 ]
+then
+clear
+pkg install php -y
+pkg install git -y
+pkg install python2 -y
+git clone https://github.com/FR13ND8/BRUTEFORCEnew
+cd BRUTEFORCEnew
+badh new.sh
+fi
+
+if [ $pil = 2 ]
+then
+clear
+git clone https://github.com/zlucifer/troll_project
+cd troll_project
+bash troll.sh
+fi
+
+if [ $pil = 3 ]
+then
+clear
+git clone https://github.com/zlucifer/skull_project
+cd  skull_project
+bash skull.sh
+fi
+
+if [ $pil = 4 ]
+then
+clear
+git clone https://github.com/zlucifer/termux_alice
+cd termux_alice
+fi
+
+if [ $pil = 5 ]
+then
+clear
+git clone https://github.com/zlucifer/paladin_project
+cd paladin_project
+bash,paladin.sh
+fi
+
+if [ $pil = 6 ]
+then
+clear
+git clone https://github.com/zlucifer/trap_project
+cd trap_project
+bash trap.sh
+fi
+
+if [ $pil = 7 ]
+then
+clear
+git clone https://github.com/thelinuxchoice/saycheese
+cd saycheese
+bash saycheese.sh
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "E X I T"|lolcat
+sleep 2
+figlet "Terima Kasih" |lolcat
+sleep 2
+figlet "Sudah Pakai" |lolcat
+sleep 2
+figlet "Tool Saya" |lolcat
+sleep 2
+echo $i"Bila Ada Masalah Bisa Nanya Kepada Saya Via"
+sleep 2
+echo $cy "╔─━━━━━━━★━━━━━━━─╗"
+echo $i     "๏WA : 082288907664๏"
+echo $cy "╚─━━━━━━━★━━━━━━━─╝"
+echo $bi "╔─━━━━━━━★━━━━━━━─╗"
+echo $i    "๏YOUTUBE  : E Z 01๏"
+echo $bi "╚─━━━━━━━★━━━━━━━─╝"
+exit
+fi
